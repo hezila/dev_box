8c9bbe45f0f78bbca79f44673aae355bcb95f9da
numb3r3 <wangfelix87@gmail.com>
2017-01-13 10:55:45 +0800

    init commit program.pp for supervisord module

--- puppet/modules/supervisord/manifests/program.pp
+++ puppet/modules/supervisord/manifests/program.pp
@@ -8,49 +8,98 @@
 #   directory => '/var/www/node.foo.bar/'
 # }
 #
-# === Authors
+# Document on parameters available at:
+# http://supervisord.org/configuration.html#program-x-section-settings
 #
-# Alexandre De Dommelin <adedommelin@tuxz.net>
-#
-# === Copyright
-#
-# Copyright 2013 Alexandre De Dommelin
-#
+
 define supervisord::program (
   $command,
-  $process_name             = '%(program_name)s',
-  $numprocs                 = '1',
-  $directory                = '',
-  $umask                    = '022',
-  $priority                 = '999',
-  $autostart                = true,
-  $autorestart              = true,
-  $startsecs                = '1',
-  $startretries             = '3',
-  $exitcodes                = '0,2',
-  $stopsignal               = 'TERM',
-  $stopwaitsecs             = '10',
-  $user                     = 'root',
-  $redirect_stderr          = false,
-  $stdout_logfile           = 'AUTO',
+  $ensure,                  = present,
+  $ensure_process           = 'running',
+  $process_name             = "program_${name}",
+  $numprocs                 = undef,
+  $numprocs_start           = undef,
+  $priority                 = undef,
+  $autostart                = undef,
+  $autorestart              = undef,
+  $startsecs                = undef,
+  $startretries             = undef,
+  $exitcodes                = undef,
+  $stopsignal               = undef,
+  $stopwaitsecs             = undef,
+  $stopasgroup              = undef,
+  $killasgroup              = undef,
+  $user                     = undef,
+  $redirect_stderr          = undef,
+  $stdout_logfile           = "program_${name}.log",
   $stdout_logfile_maxbytes  = '50MB',
   $stdout_logfile_backups   = '10',
   $stdout_capture_maxbytes  = '0',
-  $stdout_events_enabled    = false,
-  $stderr_logfile           = 'AUTO',
+  $stdout_events_enabled    = undef,
+  $stderr_logfile           = 'program_${name}.error',
   $stderr_logfile_maxbytes  = '50MB',
   $stderr_logfile_backups   = '10',
   $stderr_capture_maxbytes  = '0',
-  $stderr_events_enabled    = false,
-  $environment              = '',
-  $serverurl                = 'AUTO'
+  $stderr_events_enabled    = undef,
+  $environment              = undef,
+  $directory                = undef,
+  $umask                    = undef,
+  $serverurl                = undef,
+  $config_file_mode         = '0644'
 ) {
-  file { "/etc/supervisor/conf.d/${name}.conf":
-    ensure  => 'present',
+  include supervisord
+
+  # create log variable
+  $stdout_logfile_path = $stdout_logfile ? {
+                       /(NONE|AUTO|syslog)/ => $stdout_logfile,
+                       /^\//                => $stdout_logfile,
+                       default              => "${supervisord::log_path}/${stdout_logfile}",
+  }
+
+  $stderr_logfile_path = $stderr_logfile ? {
+                       /(NONE|AUTO|syslog)/ => $stderr_logfile,
+                       /^\//                => $stderr_logfile,
+                       default              => "${supervisord::log_path}/${stderr_logfile}",
+  }
+
+  # handle deprecated $environment variable
+  if $environment { notify {'[supervisord] *** DEPRECATED WARNING ***: $program_environment has replaced $environment':}}
+  $_program_environment = $program_environment ? {
+                        undef                  => $environment,
+                        default                => $program_environment
+  }
+
+
+  file { "$conf":
+    ensure  => $ensure,
     content => template('supervisord/program.conf.erb'),
     owner   => 'root',
     group   => 'root',
-    mode    => '0644',
-    require => Class['supervisord']
+    mode    => $config_file_mode,
+    content => template('supervisord/conf/program.erb'),
+    notify  => Class['supervisord::reload'],
+  }
+
+  case $ensure_process {
+       'stopped': {
+                  supervisord::supervisorctl { "stop_${name}":
+                      command => 'stop',
+                      process => $name
+                  }
+       }
+       'removed': {
+                  supervisord::supervisorctl { "remove_${name}":
+                      command => 'remove',
+                      process => $name
+                  }
+       }
+       'running': {
+                  supervisord::supervisorctsl { "start_${name}":
+                      command => 'start',
+                      process => $name,
+                      unless  => running
+                  }
+       }
+       default: {}
   }
 }
