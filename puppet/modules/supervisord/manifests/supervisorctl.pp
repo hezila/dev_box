9301c99e443939e9c55b86c3680b5d3432d5cc2e
numb3r3 <wangfelix87@gmail.com>
2017-01-13 14:02:13 +0800

    init commit supervisorctl.pp

---
+++ puppet/modules/supervisord/manifests/supervisorctl.pp
@@ -0,0 +1,32 @@
+# Define: supervisord::supervisorctl
+#
+# This defines executes command with the supervisorctl tool
+#
+define supervisord::supervisorctl(
+       $command,
+       $process     = undef,
+       $refreshonly = false,
+       $unless      = undef
+) {
+  $supervisorctl = $::supervisord::executable_ctl
+
+  if $process {
+     $cmd = join([$supervisorctl, $command, $process], ' ')
+  }
+  else {
+       $cmd = join([$supervisorctl, $command], ' ')
+  }
+
+  if $unless {
+     $unless_cmd = join([$supervisorctl, 'status', $process, '|', 'awk', '{\'print $2\'}', '|', 'grep', '-i', $unless], ' ')
+  }
+  else {
+       $unless_cmd = undef
+  }
+
+  exec { "supervisorctl_command_${name}"
+       command => $cmd,
+       refreshonly  => $refreshonly,
+       unless       => $unless_cmd
+  }
+}
